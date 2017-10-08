2998e46795a01d3a585ab31c95d4463b6024bd15
tobive <knightsofkangouw@gmail.com>
2017-10-08 23:18:20 +0700

    small nav page

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,18 @@
+//-------------------------------------------------
+//--- gif-like transition for collection of images
+//-------------------------------------------------
 var imgClass = ".anim-image";
+var imgArr = document.querySelectorAll(imgClass);
+var TIME_DELAY = 3000; //time until image changed
 
 function createAnimatedImages(animImages) {
-  var imgArr = document.querySelectorAll(animImages);
-  // imgArr[0].style.opacity = 1;
   for(let i=imgArr.length-1; i>=0; i--) {
-    // imgArr[i].classList.toggle('anim-on');
     imgArr[i].style.opacity = 1;
     console.log("put image " + i + " to on." );
     setTimeout(function() {
-      // imgArr[i].classList.remove('anim-on');
       imgArr[i].style.opacity = 0;
       console.log("remove anim from image " + i + " .");
-    }, (imgArr.length - i) * 3000);
+    }, (imgArr.length - i) * TIME_DELAY);
   }
 }
 
@@ -19,4 +20,21 @@ createAnimatedImages(imgClass);
 setInterval(function() {
   console.log("RUNNING");
   createAnimatedImages(imgClass);
-}, 9000);
+}, (imgArr.length + 1) * TIME_DELAY);
+
+//-------------------------------------------------
+//--- put click and animating nav-burger button
+//-------------------------------------------------
+var burgerButton = document.querySelector(".nav-burger");
+var crossLine1 = document.querySelector(".burger-line-1");
+var crossLine2 = document.querySelector(".burger-line-2");
+var crossLine3 = document.querySelector(".burger-line-3");
+var navPage = document.querySelector(".nav-page");
+
+burgerButton.addEventListener('click', function(e) {
+  crossLine1.classList.toggle('cross');
+  crossLine2.classList.toggle('cross');
+  crossLine3.classList.toggle('cross');
+  navPage.classList.toggle('show');
+  e.stopPropagation();
+});
