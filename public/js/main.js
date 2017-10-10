2e1e22a40b25804180ce8598c2d3d96d3aef4309
tobive <knightsofkangouw@gmail.com>
2017-10-10 11:34:02 +0700

    add document selector

--- public/js/main.js
+++ public/js/main.js
@@ -8,22 +8,22 @@ var TIME_DELAY = 3000; //time until image changed
 function createAnimatedImages(animImages) {
   for(let i=imgArr.length-1; i>=0; i--) {
     imgArr[i].style.opacity = 1;
-    console.log("put image " + i + " to on." );
+    // console.log("put image " + i + " to on." );
     setTimeout(function() {
       imgArr[i].style.opacity = 0;
-      console.log("remove anim from image " + i + " .");
+      // console.log("remove anim from image " + i + " .");
     }, (imgArr.length - i) * TIME_DELAY);
   }
 }
 
 createAnimatedImages(imgClass);
 setInterval(function() {
-  console.log("RUNNING");
+  // console.log("RUNNING");
   createAnimatedImages(imgClass);
 }, (imgArr.length + 1) * TIME_DELAY);
 
 //-------------------------------------------------
-//--- put click and animating nav-burger button
+//--- click and animation for nav-burger button
 //-------------------------------------------------
 var burgerButton = document.querySelector(".nav-burger");
 var crossLine1 = document.querySelector(".burger-line-1");
@@ -38,3 +38,72 @@ burgerButton.addEventListener('click', function(e) {
   navPage.classList.toggle('show');
   e.stopPropagation();
 });
+
+//-------------------------------------------------
+//--- load JSON content for drop-down-form
+//-------------------------------------------------
+var DATA_LOC = "data.json"; //location of json file to be loaded
+var DOC_LOC = "/documents/"; //location of documents
+
+function loadJSON(fileLocation, callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', fileLocation, true);
+  xobj.onreadystatechange = function() {
+    if(xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
+    }
+  }
+  xobj.send(null);
+}
+
+function populateDropDown(response) {
+  var data = JSON.parse(response);
+  var dropDownYear = document.querySelector("#analysis-year");
+  var dropDownDate = document.querySelector("#analysis-date");
+  var downloadButton = document.querySelector("#download-analysis");
+
+//populate dropdown for year
+  for(let i=0; i<data.data.length; i++) {
+    let optYear = document.createElement('option');
+    optYear.id = data.data[i].year;
+    optYear.value = i;
+    optYear.appendChild(document.createTextNode(optYear.id));
+    dropDownYear.appendChild(optYear);
+  }
+
+  //populate dropdown for date
+  for(let j=0; j<data.data[0].list.length; j++) {
+    let optDate = document.createElement('option');
+    optDate.id = data.data[0].list[j].date;
+    optDate.value = j;
+    optDate.appendChild(document.createTextNode(optDate.id));
+    dropDownDate.appendChild(optDate);
+  }
+
+  //attach event handler to options in year-dropdown
+  dropDownYear.addEventListener('click', function(e) {
+    dropDownDate.innerHTML = ""; //clear all child node of date-Select element
+    let arrIndex = dropDownYear.options[dropDownYear.selectedIndex].value;
+    for(let j=0; j<data.data[arrIndex].list.length; j++) {
+      let optDate = document.createElement('option');
+      optDate.id = data.data[arrIndex].list[j].date;
+      optDate.value = j;
+      optDate.appendChild(document.createTextNode(optDate.id));
+      dropDownDate.appendChild(optDate);
+    }
+    e.stopPropagation();
+  });
+
+  //attach event handler to download document button
+  downloadButton.addEventListener('click', function(e) {
+    let yearIndex = dropDownYear.options[dropDownYear.selectedIndex].value;
+    let dateIndex = dropDownDate.options[dropDownDate.selectedIndex].value;
+    let fileName = data.data[yearIndex].list[dateIndex].filename;
+    window.open(DOC_LOC + fileName, '_blank');
+    e.stopPropagation();
+  });
+}
+
+
+loadJSON(DATA_LOC, populateDropDown);
