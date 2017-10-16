8d7fac1d7cca92a9c2c8d167f4539d39d5158d24
tobive <knightsofkangouw@gmail.com>
2017-10-16 07:35:40 +0700

    edit tag in index.html

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8000;
 
-app.use(express.static('public'));
+app.use('/', express.static('public', {
+  setHeaders: function(res) {
+    // res.type('text/html; charset=UTF-8; application/json');
+    // res.setHeader('Content-Type','text/css');
+    // res.setHeader('Content-Type', 'charset=utf-8');
+    // res.setHeader('Cache-Control', 'no-cache');
+    // console.log(res.get('Content-Type'));
+  }
+}));
 
 app.listen(PORT, function() {
   console.log("Express is listening on port ", PORT);
