df0cb1cebc3129dc5c0239e6d09236873e6d0880
tobive <knightsofkangouw@gmail.com>
2017-10-30 17:06:27 +0700

    add ken2, edit docs

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(function(req, res, next) {
 })
 
 app.use(compress());
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'app')));
 app.use(bodyParser.json());
 
 
